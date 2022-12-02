9d46ef607b130f45a98e80acec9a0b06d6bb9a3f
Hughes Perreault <hughes.perreault@thalesdigitalsolutions.ca>
2022-12-01 20:04:24 -0500

    citations

--- generate_citations_plot.py
+++ generate_citations_plot.py
@@ -9,23 +9,24 @@ with plt.xkcd():
     ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
 
 
-    y = [3, 26, 37]
+    citations_by_year = [3, 26, 37]
+    y = np.cumsum(citations_by_year)
     x = ["2020", "2021", "2022"]
 
     ax.annotate(
-        'HERE THE PHD IS DONE \nAND THE THALES ADVENTURE \nSTARTS',
-        xy=("2021", 26), arrowprops=dict(arrowstyle='->'), xytext=("2021", 18))
+        'HERE THE PHD IS \nDONE AND THE THALES \nADVENTURE STARTS',
+        xy=("2021", 29), arrowprops=dict(arrowstyle='->'), xytext=("2021", 15))
 
-    ax.annotate(
-        'Total number of citations is ' + str(np.sum(y)),
-        xy=("2020", 35), xytext=("2020", 35))
+    # ax.annotate(
+    #     'Total number of citations is ' + str(np.sum(y)),
+    #     xy=("2020", 35), xytext=("2020", 35))
 
 
     ax.plot(x, y)
 
     fig.text(
         0.5, 0.05,
-        'Citations over time, last updated december 2022',
+        'Citations accumulated over time \nLast updated december 2022',
         ha='center')
 
 plt.savefig('assets/images/citations.png')
